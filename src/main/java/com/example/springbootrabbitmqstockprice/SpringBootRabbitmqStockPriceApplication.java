de81cc4c39515185418113872431dabbbef1da26
valberjr <gv0ez8ev@duck.com>
2023-01-06 15:23:49 -0300

    project creation and configuration

---
+++ src/main/java/com/example/springbootrabbitmqstockprice/SpringBootRabbitmqStockPriceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootrabbitmqstockprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqStockPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRabbitmqStockPriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootrabbitmqstockprice/SpringBootRabbitmqStockPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootrabbitmqstockprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqStockPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
